cc439bc315a0c0256640ef3e46889fd5e51fb726
cyberplanner <saleh.gfx@gmail.com>
2016-09-09 14:26:31 +0100

    Adds a github controller files.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
 
   def new
-    redirect_to '/auth/github'
+    redirect_to '/auth/github' # could be replaced with '/signin'
   end
 
   def create
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160909121755) do
+ActiveRecord::Schema.define(version: 20160909132207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "github_profiles", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "nickname"
+    t.string   "email"
+    t.string   "name"
+    t.string   "image"
+    t.string   "location"
+    t.integer  "public_repo"
+    t.integer  "public_gists"
+    t.integer  "followers"
+    t.integer  "following"
+    t.string   "member_since"
+    t.string   "access_token"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
