972c8dc8c071794a3618d2917f46ca4e289cf503
cyberplanner <saleh.gfx@gmail.com>
2016-09-09 19:08:22 +0100

    IT'S ALIVE!!

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,6 +1,11 @@
 class GithubController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user?, :except => [:index]
+
   def index
     client = Octokit::Client.new(:access_token => current_user.github_profile.access_token)
-    @repositories = client.repos
+    @repos = client.repos
   end
+
+
 end
