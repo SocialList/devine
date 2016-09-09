b0b4aad37fd59e8e0431b6aa1371733574486800
cyberplanner <saleh.gfx@gmail.com>
2016-09-09 14:23:16 +0100

    Adds a github_profile model (before migration).

---
+++ spec/factories/github_profiles.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :github_profile do
+    user_id 1
+    nickname "MyString"
+    email "MyString"
+    name "MyString"
+    image "MyString"
+    location "MyString"
+    public_repo 1
+    public_gists 1
+    followers 1
+    following 1
+    member_since "MyString"
+    access_token "MyString"
+  end
+end
