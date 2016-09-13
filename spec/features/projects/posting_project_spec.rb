ce9b47e7b07b335ae8b35a005a234fc235003e57
6eff <6eff@mail.ru>
2016-09-13 20:27:57 +0300

    tests uninqueness of projects and closes #13

--- spec/features/projects/posting_project_spec.rb
+++ spec/features/projects/posting_project_spec.rb
@@ -10,4 +10,12 @@ feature '#Posting project' do
     expect(page).to have_content 'Project created successfully'
     expect(page).to have_content "These are my atom files"
   end
+
+  context 'project was posted' do
+    scenario 'user cannot add his repo to another project' do
+      signin
+      make_project
+      expect{Project.create!(:repo_id => 66507460, :name => 'new project', :description => "nil")}.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Repo has already been taken")
+    end
+  end
 end
