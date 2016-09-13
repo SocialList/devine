d53d1a6aaa1d622a450068ce222feb90ea286325
cyberplanner <saleh.gfx@gmail.com>
2016-09-13 16:41:10 +0100

    Adds empty trash button.

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,7 +1,7 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
   before_action :get_mailbox
-  before_action :get_conversation, except: [:index]
+  before_action :get_conversation, except: [:index, :empty_trash]
   before_action :get_box, only: [:index]
 
 
@@ -23,6 +23,26 @@ class ConversationsController < ApplicationController
     current_user.reply_to_conversation(@conversation, params[:body])
     flash[:success] = 'Reply sent'
     redirect_to conversation_path(@conversation)
+  end
+
+  def destroy
+    @conversation.move_to_trash(current_user)
+    flash[:success] = "The conversation was moved to trash"
+    redirect_to conversation_path
+  end
+
+  def restore
+    @conversation.untrash(current_user)
+    flash[:success] = "The conversation was restored"
+    redirect_to conversation_path
+  end
+
+  def empty_trash
+    @mailbox.trash.each do |conversation|
+      conversation.receipts_for(current_user).update_all(deleted: true)
+    end
+    flash[:success] = "Your trash folder is now empty"
+    redirect_to conversations_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   resources :conversations, only: [:index, :show, :destroy] do
     member do
       post :reply
+      post :restore
+    end
+    collection do
+      delete :empty_trash
     end
   end
   resources :messages, only: [:new, :create]
