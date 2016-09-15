fbc49110b22e3fa2a24caaffc0dbc46539881ea8
Frunez <francesco.nunez@gmail.com>
2016-09-15 12:38:12 +0100

    Created contact owner link

--- spec/features/projects/viewing_project_spec.rb
+++ spec/features/projects/viewing_project_spec.rb
@@ -9,4 +9,20 @@ feature 'Viewing individual project' do
     expect(current_path).to eq '/projects/text-editors'
     expect(page).to have_content 'These are my atom files'
   end
+
+
+
+  scenario 'Users can contact owner with link on page' do
+    User.create!(:id => 6)
+    # User.create!(:id => 7)
+    Project.create!(:repo_id => 91437971, :name => 'This is a long project name', :description => "nil", :user_id => 6)
+    visit '/'
+    signin
+    click_link 'View project This is a long project name'
+    click_link 'Contact owner'
+    fill_in 'message_subject', with: 'Hi'
+    fill_in 'Message', with: 'Nice'
+    click_on 'Send'
+    expect(page).to have_content 'Message has been sent!'
+  end
 end
